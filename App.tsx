41b5470c4b4917699e28be6e8666616f10524eef
Alexs1313 <o.salata@o-salata.local>
2025-10-23 11:23:09 +0300

    updated

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ const App = () => {
     <NavigationContainer>
       <ContextProvider>
         {showCharmWelcomeScreen ? (
-          <Keepladysmoodstac />
+          <Keepladysmoodstack />
         ) : (
           <KeepLadysMoodCharmLoader />
         )}
